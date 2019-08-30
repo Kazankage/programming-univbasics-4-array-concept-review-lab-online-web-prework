b19881fbed9a4b91f96b6d432904f0e9d9fb7ef1
Randel Jiao <randeljiao@gmail.com>
2019-08-30 21:15:30 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
   index = 0
   while index < array.length do
-    if array[index] == value_to_find
+    if value_to_find == array[index]
       return index
     else
       return nil
